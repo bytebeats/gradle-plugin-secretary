24b1bc09688bf0799482a21cfd85d486440cbfb8
panchen <panchen@tigerbrokers.com>
2021-08-07 17:08:57 +0800

    ConfigureStep and NewGroovyClassAction.kt

--- src/main/kotlin/me/bytebeats/agp/secretary/GradlePluginModuleModel.kt
+++ src/main/kotlin/me/bytebeats/agp/secretary/GradlePluginModuleModel.kt
@@ -1,11 +1,10 @@
 package me.bytebeats.agp.secretary
 
-import com.android.tools.idea.npw.template.TemplateResolver
 import com.android.tools.idea.observable.core.BoolValueProperty
 import com.android.tools.idea.observable.core.StringValueProperty
 import com.android.tools.idea.wizard.model.WizardModel
+import com.android.utils.FileUtils
 import com.intellij.openapi.project.Project
-import org.gradle.buildinit.plugins.internal.TemplateValue
 import java.io.File
 
 
@@ -18,20 +17,41 @@ import java.io.File
  * @Description TO-DO
  */
 
-class GradlePluginModuleModel(val project: Project, val templateDir: File) ://val templateHandle: TemplateHandle,
+class GradlePluginModuleModel(val project: Project, val templateDirectory: File?) ://val templateHandle: TemplateHandle,
         WizardModel() {
     val pluginName = StringValueProperty("plugin")
     val packageName = StringValueProperty()
     val className = StringValueProperty("MyPlugin")
     val createGitIgnore = BoolValueProperty(true)
     override fun handleFinished() {
-        TODO("Not yet implemented")
+        createModule()
+        deleteTemplateDirectory()
+    }
+
+    override fun handleSkipped() {
+        super.handleSkipped()
+        deleteTemplateDirectory()
+    }
+
+    override fun dispose() {
+        super.dispose()
+        deleteTemplateDirectory()
+    }
+
+    private fun deleteTemplateDirectory() {
+        try {
+            templateDirectory?.let {
+                FileUtils.deletePath(it)
+            }
+        } catch (ignore: Exception) {
+
+        }
     }
 
     fun createModule() {
         val moduleRoot = File(project.basePath, pluginName.get())
         val templateValues = mutableMapOf<String, Any>()
-//        val injector =
+//        val injector = TemplateResolver.Companion.
     }
 
 
